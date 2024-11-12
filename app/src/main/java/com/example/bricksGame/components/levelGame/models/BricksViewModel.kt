b14eda967eeef05278bc3a33b1bfac235bbdf5a5
SpiritualAdviser <pomaxam@narod.ru>
2024-11-12 21:41:22 +0300

    fix addItem and deleteItem to bricksList

--- app/src/main/java/com/example/bricksGame/components/levelGame/models/BricksViewModel.kt
+++ app/src/main/java/com/example/bricksGame/components/levelGame/models/BricksViewModel.kt
@@ -7,44 +7,51 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import com.example.bricksGame.ui.theme.colorsBricks
 
-object BricksViewModel: ViewModel() {
+object BricksViewModel : ViewModel() {
 
     val width = 70.dp
     val height = 60.dp
     private val padding = 5.dp
-    private val numberBricks = 3
-    val widthGridSize = (width + padding) * numberBricks + padding
+    private val maxBricks = 3
+    val widthGridSize = (width + padding) * maxBricks + padding
     val heightGridSize = height + padding
 
     var bricksList = mutableStateListOf<Brick>()
 
     fun addItem() {
-        bricksList.add(
-            Brick(
-                width = width,
-                height = height,
-                id = 0,
-                position = "00",
-                color = getRandomColor()
-            )
-        )
-    }
-
-    fun deleteItem(index: Int) {
-        bricksList.removeAt(index)
-    }
-    fun createBricksList(): Unit {
-
-        for (i in 1..numberBricks) {
+        if (bricksList.size < maxBricks) {
             bricksList.add(
                 Brick(
                     width = width,
                     height = height,
-                    id = 0,
-                    position = "00",
+                    id = bricksList.size,
+                    position = (bricksList.size).toString(),
                     color = getRandomColor()
                 )
             )
+        }
+    }
+
+    fun deleteItem(index: Int) {
+        if (bricksList.size != 0) {
+            bricksList.removeAt(index)
+        }
+    }
+
+    fun createBricksList(): Unit {
+        if (bricksList.size < maxBricks) {
+
+            for (i in bricksList.size until maxBricks) {
+                bricksList.add(
+                    Brick(
+                        width = width,
+                        height = height,
+                        id = i,
+                        position = i.toString(),
+                        color = getRandomColor()
+                    )
+                )
+            }
         }
     }
 
