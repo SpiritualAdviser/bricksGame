ff5bd91ae2af474ff8d13776aae2ed37c4f270ec
SpiritualAdviser <pomaxam@narod.ru>
2024-11-16 10:28:56 +0300

    add runCollision

--- app/src/main/java/com/example/bricksGame/ui/helper/Collision.kt
+++ app/src/main/java/com/example/bricksGame/ui/helper/Collision.kt
@@ -1,9 +1,16 @@
 package com.example.bricksGame.ui.helper
 
+import androidx.core.view.ContentInfoCompat.Flags
+
 class Collision<T, V> {
 
     private lateinit var targetsList: MutableList<T>
     private lateinit var itemsList: MutableList<V>
+    private var isStarted = false
+
+    fun runCollision(state: Boolean) {
+        isStarted = state
+    }
 
     fun addToCollision(target: T? = null, item: V? = null) {
 
@@ -14,5 +21,7 @@ class Collision<T, V> {
             itemsList.add(item)
         }
     }
+
+
 }
 
