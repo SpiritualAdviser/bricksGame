9ebb40d900a4eb12a18ca222d3f186bbec98a377
SpiritualAdviser <pomaxam@narod.ru>
2024-11-27 20:12:35 +0300

    button sound is TopEnd

--- app/src/main/java/com/example/bricksGame/components/gameMeny/HomeScreenView.kt
+++ app/src/main/java/com/example/bricksGame/components/gameMeny/HomeScreenView.kt
@@ -1,32 +1,22 @@
 package com.example.bricksGame.components.gameMeny
 
 import android.content.res.Configuration
-import android.media.MediaPlayer
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.bricksGame.R
 import com.example.bricksGame.components.gameMeny.models.HomeScreenViewModel
-import com.example.bricksGame.components.levelGame.PortraitLayout
 import com.example.bricksGame.soundController
 import com.example.bricksGame.ui.GameConfig
 import com.example.bricksGame.ui.helper.ButtonController
-import com.example.bricksGame.ui.helper.SoundController
 
 @Composable
 fun RunHomeScreen() {
@@ -47,6 +37,26 @@ fun RunHomeScreen() {
             contentScale = ContentScale.Crop
         )
     }
+    Box(
+        modifier = Modifier
+            .padding(20.dp)
+            .fillMaxSize()
+
+        ) {
+        IconToggleButton(
+            checked = GameConfig.SOUND_MUTED, onCheckedChange = {
+                soundController.soundMute()
+            },
+            modifier = Modifier
+                .size(60.dp, 60.dp)
+                .align(Alignment.TopEnd)
+                .paint(
+                    painter = if (GameConfig.SOUND_MUTED) painterResource(R.drawable.play_muted)
+                    else painterResource(R.drawable.play),
+                    contentScale = ContentScale.FillWidth
+                )
+        ) {}
+    }
 
     Column(
         modifier = Modifier
@@ -60,20 +70,6 @@ fun RunHomeScreen() {
                 .size(100.dp, 80.dp)
                 .paint(
                     painter = painterResource(R.drawable.buttons_start),
-                    contentScale = ContentScale.FillWidth
-                )
-
-        ) {}
-
-        IconToggleButton(
-            checked = GameConfig.SOUND_MUTED, onCheckedChange = {
-                soundController.soundMute()
-            },
-            modifier = Modifier
-                .size(60.dp, 60.dp)
-                .paint(
-                    painter = if (GameConfig.SOUND_MUTED) painterResource(R.drawable.play_muted)
-                    else painterResource(R.drawable.play),
                     contentScale = ContentScale.FillWidth
                 )
         ) {}
