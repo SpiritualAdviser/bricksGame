98ab017f656372bc57f33c8a8c17f5fd962edb18
SpiritualAdviser <pomaxam@narod.ru>
2024-12-14 14:19:14 +0300

    add Levels icon

--- app/src/main/java/com/example/bricksGame/components/Map/Map.kt
+++ app/src/main/java/com/example/bricksGame/components/Map/Map.kt
@@ -1,30 +1,59 @@
 package com.example.bricksGame.components.Map
 
+import com.example.bricksGame.R
+import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.compose.errorContainerLight
+
 
 @Composable
 fun Map() {
-    Box(Modifier.fillMaxSize(),
+    Box(
+        Modifier.fillMaxSize(),
 
-     ) {
-     LazyVerticalGrid(
-      modifier = Modifier.fillMaxSize()
-       .background(errorContainerLight)
-       .padding(50.dp)
-      ,
-      columns = GridCells.Adaptive(20.dp)
+        ) {
+        LazyVerticalGrid(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(errorContainerLight)
+                .padding(50.dp),
+            columns = GridCells.Adaptive(minSize = 50.dp),
+            verticalArrangement = Arrangement.spacedBy(15.dp),
+            horizontalArrangement = Arrangement.spacedBy(15.dp)
+        ) {
+            items(100) {
+                LevelMap()
+            }
+        }
+    }
+}
 
-     ) {
-
-     }
+@Composable
+fun LevelMap() {
+    IconButton(
+        onClick = { },
+        modifier = Modifier
+            .size(50.dp, 60.dp)
+            .paint(
+                painter = painterResource(R.drawable.map_levels_bg),
+                contentScale = ContentScale.FillBounds
+            )
+    ) {
+        Text("1", fontSize = 20.sp)
     }
 }
