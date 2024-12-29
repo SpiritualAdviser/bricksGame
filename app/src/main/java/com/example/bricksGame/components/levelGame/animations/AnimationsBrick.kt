f5faaf1c03e3bc20737e59e1ea55c6917f18904e
SpiritualAdviser <pomaxam@narod.ru>
2024-12-29 11:24:57 +0300

    fix Translation bricks on resize

--- app/src/main/java/com/example/bricksGame/components/levelGame/animations/AnimationsBrick.kt
+++ app/src/main/java/com/example/bricksGame/components/levelGame/animations/AnimationsBrick.kt
@@ -10,6 +10,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.time.Duration
 
 object AnimationsBrick {
     var canRunTranslation = mutableStateOf(false)
@@ -47,9 +48,9 @@ object AnimationsBrick {
         brick.delayTranslation = 250 * (index + 1)
 
         CoroutineScope(Dispatchers.Main).launch {
-            delay(500)
+
+            delay((brick.delayTranslation + 200).toLong())
             brick.wasAnimated.value = true
         }
     }
-
 }
