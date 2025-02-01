3b1d2042dfdfe371ca4f71be918572c0f28c25dc
SpiritualAdviser <pomaxam@narod.ru>
2025-02-01 19:09:11 +0300

    add hilt id("com.google.dagger.hilt.android") version "2.55" apply false

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,6 @@ plugins {
     alias(libs.plugins.devtoolsKsp)
     kotlin("jvm") version "2.1.0"
     kotlin("plugin.serialization") version "2.1.0"
+
+    id("com.google.dagger.hilt.android") version "2.55" apply false
 }
