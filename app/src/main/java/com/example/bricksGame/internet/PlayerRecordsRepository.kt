67e6f04d11f15c9a1a132b94bbe33a15b4390435
SpiritualAdviser <pomaxam@narod.ru>
2025-02-17 20:42:06 +0300

    add isOnline

--- app/src/main/java/com/example/bricksGame/internet/PlayerRecordsRepository.kt
+++ app/src/main/java/com/example/bricksGame/internet/PlayerRecordsRepository.kt
@@ -1,9 +1,14 @@
 package com.example.bricksGame.internet
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.util.Log
 import androidx.compose.runtime.mutableStateListOf
 import com.example.bricksGame.components.players.data.Player
 import com.example.bricksGame.components.players.repository.PlayerRepository
+import dagger.hilt.android.qualifiers.ActivityContext
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -12,7 +17,10 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class PlayerRecordsRepository @Inject constructor() {
+class PlayerRecordsRepository @Inject constructor(
+    @ApplicationContext val context: Context
+
+) {
     init {
         Log.d("my", "PlayersRecordsRepository")
     }
@@ -36,6 +44,10 @@ class PlayerRecordsRepository @Inject constructor() {
     private var dataPlayerRecords: DataPlayerRecords = defaultPlayerRecords
 
     fun getRecords() {
+        if (!isOnline(context)) {
+            return
+        }
+
         CoroutineScope(Dispatchers.IO).launch {
             val result = apiService.getData()
             if (result.isSuccessful) {
@@ -57,6 +69,9 @@ class PlayerRecordsRepository @Inject constructor() {
     }
 
     fun setRecords(player: Player) {
+        if (!isOnline(context)) {
+            return
+        }
 
         val newPlayerRecords =
             PlayerAchievement(
@@ -90,5 +105,25 @@ class PlayerRecordsRepository @Inject constructor() {
 
     fun setActivePlayer(player: Player) {
         activePlayer = player
+    }
+
+    private fun isOnline(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                return true
+            }
+        }
+        return false
     }
 }
