899934187b72e345f391e7ebdaeb29e00b3ef16a
SpiritualAdviser <pomaxam@narod.ru>
2025-02-23 21:12:11 +0300

    split classes Dictionary

---
+++ app/src/main/java/com/example/bricksGame/localization/Dictionary.kt
@@ -0,0 +1,66 @@
+package com.example.bricksGame.localization
+
+class Dictionary {
+    val en = mapOf(
+        "adventure" to "Adventure",
+        "russian" to "Russian",
+        "english" to "English",
+        "german" to "German",
+        "survival" to "Survival",
+        "players" to "Players",
+        "records" to "Records",
+        "info" to "Info",
+        "snackAddPlayer" to "The Player is added",
+        "snackEmptyNamePlayer" to "The name of Player can not be empty",
+        "createPlayer" to "Create Player",
+        "achiev" to "Achiev",
+        "enterPlayerName" to "Enter player name",
+        "labelName" to "name",
+        "target" to "Target",
+        "winLine" to "WinLine",
+        "step" to "Step",
+        "score" to "Score",
+    )
+
+    val ru = mapOf(
+        "adventure" to "Приключения",
+        "russian" to "Русский",
+        "english" to "Английский",
+        "german" to "Немецкий",
+        "survival" to "Выживание",
+        "players" to "Игроки",
+        "records" to "Рекорды",
+        "info" to "Информация",
+        "snackAddPlayer" to "Игрок добавлен",
+        "snackEmptyNamePlayer" to "Заполните Имя Игрока",
+        "createPlayer" to "Создать Игрока",
+        "achiev" to "Рекорды",
+        "enterPlayerName" to "Имя игрока",
+        "labelName" to "имя",
+        "target" to "Сбросить",
+        "winLine" to "Линия выигрыша",
+        "step" to "Ходы",
+        "score" to "Счёт",
+    )
+
+    val gm = mapOf(
+        "adventure" to "Abenteuer",
+        "russian" to "Russisch",
+        "english" to "Englisch",
+        "german" to "Deutsch",
+        "survival" to "Überleben",
+        "players" to "Spieler",
+        "records" to "Rekorde",
+        "info" to "Info",
+        "snackAddPlayer" to "Spieler hinzugefügt",
+        "snackEmptyNamePlayer" to "Füllen Sie den Namen des Spielers aus",
+        "createPlayer" to "Spieler erstellen",
+        "achiev" to "Rekorde",
+        "enterPlayerName" to "Name des Spielers",
+        "labelName" to "name",
+        "target" to "Abwerfen",
+        "winLine" to "Gewinnlinie",
+        "step" to "Verläufe",
+        "score" to "Spielstand",
+    )
+}
